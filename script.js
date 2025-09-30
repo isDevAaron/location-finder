ddfbe037b5a378a9dd8b237c2614e6c7e90f2921
isDevAaron <63882594+isDevAaron@users.noreply.github.com>
2025-09-29 20:27:12 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Replace with your Google Apps Script Web App URL
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbztwB7Ck8wn-Q7juRGugzFWhLk7Gx_lDTnEH5RKA-dKzFxlCJK8ZH_HoBGXGjJojw5lTw/exec";
+
 // Gather device info
 function getDeviceInfo() {
     return {
@@ -11,13 +14,25 @@ function getDeviceInfo() {
     };
 }
 
-// Send log to server
-fetch('/log', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-        userAgent: navigator.userAgent,
-        referrer: document.referrer,
-        device: getDeviceInfo()
+// Fetch visitor IP and location
+fetch("https://ipapi.co/json/")
+    .then(res => res.json())
+    .then(data => {
+        const payload = {
+            ip: data.ip,
+            city: data.city,
+            region: data.region,
+            country: data.country_name,
+            userAgent: navigator.userAgent,
+            referrer: document.referrer,
+            device: getDeviceInfo()
+        };
+
+        // Send data to Google Sheets via Apps Script
+        fetch(APPS_SCRIPT_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload)
+        }).catch(err => console.warn("Could not log visit:", err));
     })
-});
+    .catch(err => console.error("Could not fetch IP info:", err));
